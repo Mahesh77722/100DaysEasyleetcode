979a20d873c82e81ecd4061262bae856ce73cc09
Mahesh Anil Maheshwari <maheshmaheshwari179@gmail.com>
2024-10-06 00:08:25 +0530

    Day 1

---
+++ 100DaysEasyLeetCode/100DaysEasyLeetCode/LeetCode/Day1_Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100DaysEasyLeetCode.LeetCode
+{
+    public class Day1_Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int reminder = target - nums[i];
+                if (result.ContainsKey(reminder))
+                    return new int[] { result[reminder], i };
+                else
+                    result[nums[i]] = i;
+            }
+            return new int[] { };
+        }
+    }
+}
--- 100DaysEasyLeetCode/100DaysEasyLeetCode/Program.cs
+++ 100DaysEasyLeetCode/100DaysEasyLeetCode/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace _100DaysEasyLeetCode
+﻿using _100DaysEasyLeetCode.LeetCode;
+namespace _100DaysEasyLeetCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Day1_Solution day1 = new Day1_Solution();
             Console.WriteLine("Hello, World!");
+            day1.TwoSum(new int[] { 2,7,11,15},9);
+            Console.ReadKey();
         }
     }
 }
