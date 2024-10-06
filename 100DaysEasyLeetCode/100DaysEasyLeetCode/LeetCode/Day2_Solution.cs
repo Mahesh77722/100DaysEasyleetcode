d34df3204d5b7cff89bb2225f4a8517b096fc7c3
Mahesh Anil Maheshwari <maheshmaheshwari179@gmail.com>
2024-10-06 23:48:51 +0530

    Day 2 - Palindrome Number

---
+++ 100DaysEasyLeetCode/100DaysEasyLeetCode/LeetCode/Day2_Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100DaysEasyLeetCode.LeetCode
+{
+    public class Day2_Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            int rev, sum = 0;
+            int temp = x;
+            while (x > 0)
+            {
+                rev = x % 10;
+                sum = (sum * 10) + rev;
+                x = x / 10;
+            }
+            return temp == sum;
+        }
+    }
+}
--- 100DaysEasyLeetCode/100DaysEasyLeetCode/Program.cs
+++ 100DaysEasyLeetCode/100DaysEasyLeetCode/Program.cs
@@ -6,8 +6,10 @@ namespace _100DaysEasyLeetCode
         static void Main(string[] args)
         {
             Day1_Solution day1 = new Day1_Solution();
+            Day2_Solution day2 = new Day2_Solution();
             Console.WriteLine("Hello, World!");
             day1.TwoSum(new int[] { 2,7,11,15},9);
+            day2.IsPalindrome(1543451);
             Console.ReadKey();
         }
     }
